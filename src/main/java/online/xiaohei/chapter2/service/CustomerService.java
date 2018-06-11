35e065ccae0b6c3baac8fa193bce1d10b47a18af
xiaohei_zhang <foxz@qq.com>
2018-06-11 09:21:42 +0800

    32

--- src/main/java/online/xiaohei/chapter2/service/CustomerService.java
+++ src/main/java/online/xiaohei/chapter2/service/CustomerService.java
@@ -6,23 +6,33 @@ import java.util.List;
 import java.util.Map;
 
 public class CustomerService {
+    public List<Customer> getCustomerList() {
+//        TODO
+        return null;
+    }
+
     public List<Customer> getCustomerList(String keyword) {
+//        TODO
         return null;
     }
 
     public Customer getCustomer(long id) {
+//        TODO
         return null;
     }
 
     public boolean createCustomer(Map<String, Object> fieldMap) {
+//        TODO
         return false;
     }
 
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+//        TODO
         return false;
     }
 
     public boolean deleteeCustomer(long id) {
+//        TODO
         return false;
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/customer_create.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zy
+  Date: 2018/6/11
+  Time: 9:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page pageEncoding="utf-8" contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>客户管理 - 创建客户</title>
+</head>
+<body>
+<h1>创建客户界面</h1>
+<%--TODO--%>
+</body>
+</html>
