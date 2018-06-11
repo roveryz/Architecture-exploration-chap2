98b2b4ea478da9e36b834f283f31424448416566
xiaohei_zhang <foxz@qq.com>
2018-06-11 10:03:08 +0800

    36

---
+++ src/main/java/online/xiaohei/chapter2/util/CastUtil.java
@@ -0,0 +1,15 @@
+package online.xiaohei.chapter2.util;
+
+public class CastUtil {
+    public static int castInt(String property) {
+//        TODO
+        return 0;
+    }
+
+
+    public static boolean castBoolean(String property) {
+//        TODO
+        return false;
+    }
+
+}
